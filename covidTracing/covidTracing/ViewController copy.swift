6cf91ff495baa9db820a2b087daf734d85922c58
Akshay Talkad <akshaytalkad@MacBook-Pro.hsd1.ca.comcast.net>
2020-09-18 17:54:45 -0700

    Login and Create Account

---
+++ covidTracing/covidTracing/ViewController copy.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covidTracing
+//
+//  Created by Siddharth on 9/18/20.
+//  Copyright © 2020 SAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

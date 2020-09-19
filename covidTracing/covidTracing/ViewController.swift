99922fd245d45c724c6081aa22897d363781922c
Akshay Talkad <akshaytalkad@MacBook-Pro.hsd1.ca.comcast.net>
2020-09-18 21:50:51 -0700

    Added to UI

--- covidTracing/covidTracing/ViewController.swift
+++ covidTracing/covidTracing/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBOutlet var createButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureButtons()
+    }
+    
+    func configureButtons(){
+        loginButton.layer.cornerRadius = 0.1 * loginButton.bounds.size.width
+        loginButton.clipsToBounds = true
+        createButton.layer.cornerRadius = 0.1 * loginButton.bounds.size.width
+        createButton.clipsToBounds = true
     }
 
 

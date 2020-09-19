3f00c54acca5e9236c4f96ff9895c491bd15d4aa
Siddharth Cherukupalli <cherukusid@gmail.com>
2020-09-19 16:45:52 -0700

    Added code for red and yellow dots

--- covidTracing/covidTracing/MapViewController.swift
+++ covidTracing/covidTracing/MapViewController.swift
@@ -9,21 +9,90 @@
 import CoreLocation
 import MapKit
 import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
 
-
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    var red: Bool = false
+    var yellow: Bool = false
     
     
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        checkLocationServices()
         // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        // checkLocationServices()
+        let db = Firestore.firestore()
+        
+        var circleArray: [MKCircle] = []
+        
+        // This is the code for the red zones.
+        db.collection("stores").whereField("infectedVisitorCount", isGreaterThan: 0)
+               .getDocuments() { (querySnapshot, error) in
+                   if let error = error {
+                       print("Whoops! There was an error pulling up the documents: \(error)")
+                   } else {
+                    self.red = true
+                    
+                    for document in querySnapshot!.documents {
+                        if let geo = document.get("geolocation") as? [Double]{
+                        if let name = document.get("name") as? String{
+                            print("infected: \(name)")
+                        let annot = MKPointAnnotation()
+                        annot.coordinate = CLLocationCoordinate2D(latitude: geo[0], longitude: geo[1])
+                        annot.title = name
+                        annot.subtitle = "Infected"
+                        self.mapView.addAnnotation(annot)
+                            let redCircle: MKCircle = MKCircle(center: CLLocationCoordinate2D(latitude: geo[0], longitude: geo[1]), radius: CLLocationDistance(100))
+                            circleArray.append(redCircle)
+                            self.mapView.addOverlay(redCircle)
+                        }
+                    }
+                    
+                }
+           }
     }
+
+        //This is the code to put yellow zones on the map.
+        db.collection("stores").whereField("safety", isEqualTo: "crowd")
+            .getDocuments() { (querySnapshot, error) in
+                if let error = error {
+                    print("Whoops! There was an error pulling up the documents: \(error)")
+                } else {
+                    self.yellow = true
+                    for document in querySnapshot!.documents {
+                        if let geo = document.get("geolocation") as? [Double]{
+                        if let name = document.get("name") as? String{
+                            print("Busy: \(name)")
+                        let yellowDots = MKPointAnnotation()
+                            yellowDots.coordinate = CLLocationCoordinate2D(latitude: geo[0], longitude: geo[1])
+                            yellowDots.title = name
+                            yellowDots.subtitle = "Busy"
+                            self.mapView.addAnnotation(yellowDots)
+                            let yellowCircle: MKCircle = MKCircle(center: CLLocationCoordinate2D(latitude: geo[0], longitude: geo[1]), radius: CLLocationDistance(100))
+                            circleArray.append(yellowCircle)
+                            self.mapView.addOverlay(yellowCircle)
+                    }
+                }
+        }
+        
+}
+                
+        }
+    }
+    
+    func getInfected() -> [[Int]] {
+        var finGeo: [[Int]] = [[]]
+        let db = Firestore.firestore()
+    }
+        
+        
     
     func setupLocationManager() {
         locationManager.delegate = self
@@ -60,14 +129,16 @@ class MapViewController: UIViewController {
 }
 
 
-extension MapViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //Be back
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        //Be back
-    }
-}
+//extension MapViewController: CLLocationManagerDelegate {
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        //Be back
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        //Be back
+//    }
+//}
+//
+
 

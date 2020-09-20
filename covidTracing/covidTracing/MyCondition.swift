3dc7968b8bb59a4ae486d0c18922df6c902747a9
Akshay Talkad <akshaytalkad@MacBook-Pro.hsd1.ca.comcast.net>
2020-09-19 23:57:08 -0700

    My Condition

---
+++ covidTracing/covidTracing/MyCondition.swift
@@ -0,0 +1,18 @@
+//
+//  MyCondition.swift
+//  covidTracing
+//
+//  Created by Akshay Talkad on 9/19/20.
+//  Copyright © 2020 SAS. All rights reserved.
+//
+
+import SwiftUI
+
+class MyCondition: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
